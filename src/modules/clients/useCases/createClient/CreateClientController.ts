c91e294b37f0040d36c24088d7c2ab82873433a4
victordias <victoroliveirad96@gmail.com>
2022-05-27 12:24:48 -0300

    fix(client): add 'role' column to client's table
    Instead creates a new table to delivery man authentication, was added
    a new column calls "role" in clients's table. Like this, dividing the
    levels of regular users and deliveryman

--- src/modules/clients/useCases/createClient/CreateClientController.ts
+++ src/modules/clients/useCases/createClient/CreateClientController.ts
@@ -3,10 +3,14 @@ import { CreateClientUseCase } from './CreateClientUseCase'
 
 export class CreateClientController {
   async handle(request: Request, response: Response) {
-    const { username, password } = request.body
+    const { username, password, role } = request.body
 
     const createClientUseCase = new CreateClientUseCase()
-    const result = await createClientUseCase.execute({ username, password })
+    const result = await createClientUseCase.execute({
+      username,
+      password,
+      role
+    })
 
     return response.json(result)
   }
--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,12 @@ import express from 'express'
 import 'express-async-errors'
 import { routes } from './routes'
 import { handleError } from './middlewares/error'
-// import { clientRoutes, authRoutes } from './routes'
 
 const app = express()
 
 app.use(express.json())
 
 app.use(routes)
-
-// app.use('/clients', clientRoutes)
-// app.use('/auth', authRoutes)
 
 app.use(handleError)
 
